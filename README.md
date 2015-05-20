7503842adb278704c8670c5fa14c9235a48b6fc7
JCron245 <jonnmb@gmail.com>
2015-05-19 19:14:24 -0600

    mean wreed

---
+++ README.md
@@ -0,0 +1,9 @@
+##mean-wreed 0.1.1
+wreetco meanjs custom seed
+
+###usage
+After installing, simply install npm and bower dependencies and you are ready to go. 
+
+`npm install`
+
+`bower install`
---
+++ public/ng-apps/main/app.js
@@ -0,0 +1,45 @@
+angular.module("meanWreed", ['ngRoute', 'AppCtrl'])
+
+// configuration
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/index.html',
+		controller: 'AppCtrl'
+	})
+	// end /
+	.when('/contact', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/contact.html',
+		controller: 'AppCtrl'
+	})
+	// end /contact
+	.when('/services', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/services.html',
+		controller: 'AppCtrl'
+	})
+	// end /contact
+	.when('/rates', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/rates.html',
+		controller: 'AppCtrl'
+	})
+	// end /contact
+	.when('/map', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/map.html',
+		controller: 'AppCtrl'
+	})
+	// end /contact
+	.when('/pictures', {
+		// #/test
+		templateUrl: '/ng-apps/main/views/pictures.html',
+		controller: 'AppCtrl'
+	})
+	// end /contact
+	; // end $routeProvider statement
+	// end project route
+	// end routes
+}]);
